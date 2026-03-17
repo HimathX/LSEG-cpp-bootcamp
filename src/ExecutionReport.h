9c7db34ec01764e44dbaebd33ecacfc4024a0ee1
Janidu Pabasara <jpabasara745@gmail.com>
2026-03-17 20:20:43 +0530

    Optional strings added set default values

--- src/ExecutionReport.h
+++ src/ExecutionReport.h
@@ -1,14 +1,19 @@
 #pragma once
+#include <optional>
 #include <string>
 
 struct ExecutionReport {
     std::string orderID;
     std::string clientOrderID;
     std::string instrument;
-    int side; // 1 for Buy, 2 for Sell
-    double price;
-    int quantity;
-    int status; // 0=New, 1=Rejected, 2=Fill, 3=PFill
+    int side = 0; // 1 for Buy, 2 for Sell
+    double price = 0.0;
+    int quantity = 0;
+    int status = 0; // 0=New, 1=Rejected, 2=Fill, 3=PFill
     std::string reason;
     std::string transactionTime; // YYYYMMDD-HHMMSS.sss
+    
+    std::optional<std::string> sideText;
+    std::optional<std::string> quantityText;
+    std::optional<std::string> priceText;
 };
